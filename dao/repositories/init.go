fa9c13e2736872d9b673c1850988ffcab4218d38
Dan Chen <dan.chen@boardware.com>
2024-03-14 11:14:01 +0800

    feat

--- dao/repositories/init.go
+++ dao/repositories/init.go
@@ -6,14 +6,14 @@ import (
 )
 
 func init() {
-	ioc.GetDBInstance().AutoMigrate(
-		&entities.Merchant{},
-		&entities.SubAccount{},
-		&entities.VerificationCode{},
-		&entities.Space{},
-		&entities.Table{},
-		&entities.Food{},
-		&entities.Printer{},
-		&entities.Order{},
-		&entities.Bill{})
+	db := ioc.GetDBInstance()
+	db.AutoMigrate(&entities.Merchant{})
+	db.AutoMigrate(&entities.SubAccount{})
+	db.AutoMigrate(&entities.VerificationCode{})
+	db.AutoMigrate(&entities.Space{})
+	db.AutoMigrate(&entities.Table{})
+	db.AutoMigrate(&entities.Food{})
+	db.AutoMigrate(&entities.Printer{})
+	db.AutoMigrate(&entities.Order{})
+	db.AutoMigrate(&entities.Bill{})
 }
